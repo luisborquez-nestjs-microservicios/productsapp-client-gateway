cdc121288acadc7cfcd63099a1345f7ce0535fe0
Luis Fernando Bórquez <luis.borquezu@outlook.com>
2026-03-26 12:01:57 -0700

    feat: add PAID status to OrderStatus enum and list

--- src/orders/enum/order.enum.ts
+++ src/orders/enum/order.enum.ts
@@ -1,11 +1,13 @@
 export enum OrderStatus {
     PENDING = 'PENDING',
+    PAID = 'PAID',
     DELIVERED = 'DELIVERED',
     CANCELLED = 'CANCELLED',
 }
 
 export const OrderStatusList = [
     OrderStatus.PENDING,
+    OrderStatus.PAID,
     OrderStatus.DELIVERED,
     OrderStatus.CANCELLED,
 ];
